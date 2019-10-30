d41e6475372d73b3643e1ca53875666a8345f57b
Sahil Singh <sahil.dogstar@gmail.com>
2019-10-30 14:47:07 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Code Snippets',
+      theme: new ThemeData(primarySwatch: Colors.red),
+      home: new GetCurrentLocation(),
+    );
+  }
+}
